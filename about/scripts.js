66dc383f96fad27b5cbc823153015f1d0a700630
Vaclav Pelisek <peldax@gmail.com>
2017-04-21 21:05:53 +0200

    some seo

--- about/scripts.js
+++ about/scripts.js
@@ -7,77 +7,36 @@ function menuToggle()
 	p.classList.toggle('panel-transition');
 }
 
-var index = 1;
+var index = 0;
 
-var contentfirst = `
-<div id="header">
-	<p id="name">Václav Pelíšek <span id="twitter">@peldax</span></p>
-	<p>Developer. Climber. Perfectionist.</p>
-	<a href="mailto:info@peldax.com" target="_top">info@peldax.com</a>
-</div>
-`;
+var arrowF;
+var arrowB;
 
-var contentsecond = `
-<div id="intro">
-	<p>
-I am a software / web developer from the Czech Republic, currently studying for a Bachelor’s degree in Cybernetics and Control Engineering. I am also a member of the Georgiev Lab, a research laboratory at the University of West Bohemia. My passion lies in mathematics, especially mathematical analysis, which improves my ability to solve complex algorithmization problems. My approach involves using intuition hand in hand with careful analysis of a given task. I am a perfectionist with a strong enthusiasm for pinpointing the best solutions for non-trivial problems.
-	</p>
-</div>
-`;
+var contents;
 
-var contentthird = `
-<div id="intro">
-	<p>
-TBA
-	</p>
-</div>
-`;
+document.addEventListener("DOMContentLoaded", function(event)
+{
+    arrowF = document.getElementById('forward');
+    arrowB = document.getElementById('back');
+
+    contents = document.getElementsByClassName('content-item');
+});
 
 function changeContent()
 {
-	var t = document.getElementById('content');
-
-	switch (index)
-	{
-	    case 3:
-	    {
-		    t.innerHTML = contentthird;
-	    }
-        case 2:
-        {
-            t.innerHTML = contentsecond;
-        }
-        case 1:
-        {
-            t.innerHTML = contentfirst;
-        }
-    }
-}
-
-function activateArrow(id)
-{
-	var a = document.getElementById(id);
-
-	a.classList.remove('arrow-unavailable');
-}
-
-function deactivateArrow(id)
-{
-	var a = document.getElementById(id);
-
-	a.classList.add('arrow-unavailable');
+    Array.from(contents).forEach(function(item)
+    {
+        item.setAttribute('style', 'display:none')
+    });
+	contents[index].setAttribute('style', '');
 }
 
 function switchContentForward()
 {
 	switch (index)
 	{
-		case 1:
-		activateArrow('back'); break;
-		
-		case 2:
-		deactivateArrow('forward'); break;
-
+		case 0: arrowB.classList.remove('arrow-unavailable'); break;
+		case 1: arrowF.classList.add('arrow-unavailable'); break;
 		default: return;
 	}
 	++index;
@@ -88,13 +47,9 @@ function switchContentBack()
 {
 	switch (index)
 	{
-		case 2:
-		deactivateArrow('back'); break;
-		
-		case 3:
-		activateArrow('forward'); break;
-
-		default: return;
+        default: return;
+        case 1: arrowB.classList.add('arrow-unavailable'); break;
+		case 2: arrowF.classList.remove('arrow-unavailable'); break;
 	}
 	--index;
 	changeContent();
